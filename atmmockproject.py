5a87580d63980f5cd83acbaf8c934db86e9baa21
Olanrewaju Alaba <77321169+chryzcodez@users.noreply.github.com>
2021-04-22 10:34:07 +0100

    Update atmmockproject.py

--- atmmockproject.py
+++ atmmockproject.py
@@ -10,11 +10,69 @@ currenttime = now.strftime("%H:%M:%S")
 def generatedaccountnumber():
     return random.randrange(1111111111, 9999999999)
 
+
+def register():
+    print('\n== === *** Register Here With Us *** === ==\n')
+    first_name = input('Fill in your first name: \n')
+    if first_name == '':
+        print('No name, Please put in the your name')
+        register()
+    if len(first_name) < 2:
+        print('Your name should be more than two letters, try again')
+        register()
+    else:
+        last_name = input('Fill in your last name: \n')
+        if last_name == '':
+            print('No name, Please put in the your name')
+            register()
+        if len(last_name) < 2:
+            print('Your name should be more than two letters, try again')
+            register()
+        else:
+            email = input('Input your E-mail address: \n')
+            if email == '':
+                print('No email, Please put in the your email')
+                register()
+            if len(email) < 6:
+                print('Your email should be more than 6 letters, try again')
+                register()
+            else:
+                password = input('Generate your password: \n')
+                if password == '':
+                    print('No password, Please put in a password')
+                    register()
+                if len(str(password)) < 4:
+                    print('Your password should be more than four characters, try again')
+                    register()
+                else:
+                    accountnumber = generatedaccountnumber()
+                    print(f'=== == --- Welcome, you have an account created on the {currentday} {currenttime} --- === ==\n')
+                    print('=== == Here is your account number ***%d*** == ===\n' %accountnumber)
+                    print('== -- Keep your details a secret, most especially your **account number** and **password** -- ==\n')
+                    database[accountnumber] = [first_name, last_name, email, password]
+                    login()
+
+
+
 def login():
     print('== === *** Login Page *** === ==\n')
     print('Enter your account details below!\n')
-    accountnumberfromuser = int(input('Please put in your registered account number: \n'))
-    password = input('Put in your password carefully: \n')
+    try:
+        accountnumberfromuser = int(input('Please put in your registered account number: \n'))
+    except:
+        print('\nInvalid input')
+        luck = input('\nPress (y) to login in again or press (n )to logout')
+        if luck == 'y':
+            login()
+        elif luck == 'n':
+            quit()
+        else:
+            print('Invalid')
+
+    try:
+        password = input('Put in your password carefully: \n')
+    except:
+        print('Invalid input')
 
     for accountnumber, userdetails in database.items():
         if(accountnumber == accountnumberfromuser):
@@ -22,14 +80,14 @@ def login():
                 bankoperation(userdetails)
                 isloginsuccessful = True
             else:
-                print('\nInvalid account or password')
+                print('\nInvalid password')
                 login()
         else:
-            print('\nInvalid account or password')
+            print('\nInvalid account')
             login()
 
-def bankoperation(user):
 
+def bankoperation(user):
     print(f'=== == Welcome %s , You logged in on the {currentday} {currenttime}  == ===\n'%user[1])
     selectedoption = int(input('=== == What would you like to do or assist you with? == ===\n(1) Withdrawal\n(2) Deposit\n(3) Complaint\n(4) Logout\n(5) Exit\n'))
     if (selectedoption == 1):
@@ -46,18 +104,7 @@ def bankoperation(user):
         print('Invalid option')
         bankoperation()
 
-def register():
-    print('\n== === *** Register Here With Us *** === ==\n')
-    first_name = input('Fill in your first name: \n')
-    last_name = input('Fill in your last name: \n')
-    email = input('Input your E-mail address: \n')
-    password = input('Generate your password: \n')
-    accountnumber = generatedaccountnumber()
-    print(f'=== == --- Welcome, you have an account created on the {currentday} {currenttime} --- === ==\n')
-    print('=== == Here is your account number ***%d*** == ===\n' %accountnumber)
-    print('== -- Keep your details a secret, most especially your **account number** and **password** -- ==\n')
-    database[accountnumber] = [first_name, last_name, email, password]
-    login()
+
 
 
 
